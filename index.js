23102b08f6371068f302dcdb157ea26708188841
Jannis R <mail@jannisr.de>
2016-04-26 00:28:59 +0200

    implementation :clap:

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict'
+
+module.exports = {
+	// source: https://de.wikipedia.org/wiki/Berolinismus#Stra.C3.9Fen.2C_Pl.C3.A4tze_und_Gegenden
+
+	// abbreviations
+	"zoo":             9023201, // S+U Zoologischer Garten Bhf (Berlin)
+	"alex":            9100003, // S+U Alexanderplatz Bhf (Berlin)
+	"kudamm":          9023203, // U Kurfürstendamm (Berlin)
+
+	// Berlin slang
+	"kotti":           9013102, // U Kottbusser Tor (Berlin)
+	"goerli":          9014101, // U Görlitzer Bahnhof (Berlin)
+	"wilmi":           9024202, // U Wilmersdorfer Str. (Berlin)
+	"schlesi":         9014102, // U Schlesisches Tor (Berlin)
+	"kutschi":         9086102, // U Kurt-Schumacher-Platz (Berlin)
+	"kurtschi":        9086102, // U Kurt-Schumacher-Platz (Berlin)
+	"nolle":           9056102, // U Nollendorfplatz (Berlin)
+	"nolli":           9056102, // U Nollendorfplatz (Berlin)
+	"plumpe":          9007102, // S+U Gesundbrunnen Bhf (Berlin)
+	"rostkreuz":       9120003, // S Ostkreuz Bhf (Berlin)
+
+	// renamed stations
+	"eichkamp":        9025423, // S Messe Süd (Berlin)
+	"witzleben":       9024106, // S Messe Nord/ICC (Berlin)
+	"lehrter bahnhof": 9003201, // S+U Berlin Hauptbahnhof
+}
