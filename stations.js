4256f5bd8a8bd76ffcc9d6d0b89635f0d006a42f
Jannis R <mail@jannisr.de>
2016-08-27 18:24:45 +0200

    add s/u/s+u prefixes to aliases
    This allows vbb-find-stations & vbb-stations-autocomplete to find these stations more easily.

--- stations.js
+++ stations.js
@@ -4,34 +4,34 @@ module.exports = {
 	// source: https://de.wikipedia.org/wiki/Berolinismus#Stra.C3.9Fen.2C_Pl.C3.A4tze_und_Gegenden
 
 	// abbreviations
-	"zoo":             9023201, // S+U Zoologischer Garten Bhf (Berlin)
-	"alex":            9100003, // S+U Alexanderplatz Bhf (Berlin)
-	"kudamm":          9023203, // U Kurfürstendamm (Berlin)
+	"sbahn ubahn zoo":                 9023201, // S+U Zoologischer Garten Bhf (Berlin)
+	"sbahn ubahn alex":                9100003, // S+U Alexanderplatz Bhf (Berlin)
+	"ubahn kudamm":                    9023203, // U Kurfürstendamm (Berlin)
 
 	// Berlin slang
-	"kotti":           9013102, // U Kottbusser Tor (Berlin)
-	"goerli":          9014101, // U Görlitzer Bahnhof (Berlin)
-	"wilmi":           9024202, // U Wilmersdorfer Str. (Berlin)
-	"schlesi":         9014102, // U Schlesisches Tor (Berlin)
-	"kutschi":         9086102, // U Kurt-Schumacher-Platz (Berlin)
-	"kurtschi":        9086102, // U Kurt-Schumacher-Platz (Berlin)
-	"nolli":           9056102, // U Nollendorfplatz (Berlin)
-	"plumpe":          9007102, // S+U Gesundbrunnen Bhf (Berlin)
-	"rostkreuz":       9120003, // S Ostkreuz Bhf (Berlin)
-	"ruedi":           9045101, // U Rüdesheimer Platz (Berlin)
+	"ubahn kotti":                     9013102, // U Kottbusser Tor (Berlin)
+	"ubahn goerli":                    9014101, // U Görlitzer Bahnhof (Berlin)
+	"ubahn wilmi":                     9024202, // U Wilmersdorfer Str. (Berlin)
+	"ubahn schlesi":                   9014102, // U Schlesisches Tor (Berlin)
+	"ubahn kutschi":                   9086102, // U Kurt-Schumacher-Platz (Berlin)
+	"ubahn kurtschi":                  9086102, // U Kurt-Schumacher-Platz (Berlin)
+	"ubahn nolli":                     9056102, // U Nollendorfplatz (Berlin)
+	"sbahn ubahn plumpe":              9007102, // S+U Gesundbrunnen Bhf (Berlin)
+	"sbahn rostkreuz":                 9120003, // S Ostkreuz Bhf (Berlin)
+	"ubahn ruedi":                     9045101, // U Rüdesheimer Platz (Berlin)
 
 	// renamed stations
-	"eichkamp":        			9025423, // S Messe Süd (Berlin)
-	"witzleben":       			9024106, // S Messe Nord/ICC (Berlin)
-	"lehrter bahnhof": 			9003201, // S+U Berlin Hauptbahnhof
-	"lindenthaler allee": 		9050301, // S Mexikoplatz (Berlin)
-	"unter den linden": 		9100025, // S+U Brandenburger Tor (Berlin)
-	"pape strasse": 			9058101, // S Südkreuz (Berlin)
-	"zinnowitzer strasse": 		9100009, // U Naturkundemuseum (Berlin)
-	"seidel strasse":			9086101, // U Otisstr. (Berlin)
-	"knie":						9023101, // U Ernst-Reuter-Platz (Berlin)
-	"putlitz strasse":			9001201, // S+U Westhafen (Berlin)
-	"grossgoerschen strasse":	9057101, // S Yorckstr. [S1] (Berlin)
+	"sbahn eichkamp":                  9025423, // S Messe Süd (Berlin)
+	"sbahn witzleben":                 9024106, // S Messe Nord/ICC (Berlin)
+	"sbahn ubahn lehrter bahnhof":     9003201, // S+U Berlin Hauptbahnhof
+	"sbahn lindenthaler allee":        9050301, // S Mexikoplatz (Berlin)
+	"sbahn ubahn unter den linden":    9100025, // S+U Brandenburger Tor (Berlin)
+	"sbahn pape strasse":              9058101, // S Südkreuz (Berlin)
+	"ubahn zinnowitzer strasse":       9100009, // U Naturkundemuseum (Berlin)
+	"ubahn seidel strasse":            9086101, // U Otisstr. (Berlin)
+	"ubahn knie":                      9023101, // U Ernst-Reuter-Platz (Berlin)
+	"sbahn ubahn putlitz strasse":     9001201, // S+U Westhafen (Berlin)
+	"sbahn grossgoerschen strasse":    9057101, // S Yorckstr. [S1] (Berlin)
 
-	"kienberg gaerten der welt":	9175005 // U Neue Grottkauer Str. (Berlin) [renaming planned, late 2016 / early 2017]
+	"ubahn kienberg gaerten der welt": 9175005 // U Neue Grottkauer Str. (Berlin) [renaming planned, late 2016 / early 2017]
 }
