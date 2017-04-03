de2285a5a263805fe3e6ac5f7b14d2ac3da1623f
Jannis R <mail@jannisr.de>
2017-04-03 22:25:41 +0200

    basic tests for addresses :white_check_mark:

--- test.js
+++ test.js
@@ -10,18 +10,44 @@ const places   = require('./index')
 
 
 
-test('keys', (t) => {
-	for (let key in places.stations) {
-		t.equal(tokenize(key).join(' '), key, 'Key should be in tokenized form.')
-	}
+test('stations', (t) => {
+	t.test('keys', (t) => {
+		for (let key in places.stations) {
+			t.equal(tokenize(key).join(' '), key, 'Key should be in tokenized form.')
+		}
+		t.end()
+	})
+
+	t.test('values', so(function* (t) {
+		for (let key in places.stations) {
+			const id = places.stations[key]
+			t.equal(typeof id, 'string', 'Value should be a valid id.')
+			const station = stations(id)[0]
+			t.equal(station.id, id, 'Station id should exist.')
+		}
+	}))
+
 	t.end()
 })
 
-test('values', so(function* (t) {
-	for (let key in places.stations) {
-		const id = places.stations[key]
-		t.equal(typeof id, 'string', 'Value should be a valid id.')
-		const station = stations(id)[0]
-		t.equal(station.id, id, 'Station id should exist.')
-	}
-}))
+
+
+test('addresses', (t) => {
+	t.test('keys', (t) => {
+		for (let key in places.addresses) {
+			t.equal(tokenize(key).join(' '), key, 'Key should be in tokenized form.')
+		}
+		t.end()
+	})
+
+	t.test('values', so(function* (t) {
+		for (let key in places.addresses) {
+			const address = places.addresses[key]
+			t.equal(typeof address.name, 'string')
+			t.equal(typeof address.latitude, 'number')
+			t.equal(typeof address.longitude, 'number')
+		}
+	}))
+
+	t.end()
+})
